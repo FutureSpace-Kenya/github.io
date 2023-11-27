ddb6161fa3b46ddbe50cb47ca28d7fac9e081345
Raccoon254 <tomsteve187@gmail.com>
2023-11-27 00:56:29 -0800

    Favicon fix

--- resources/js/script.js
+++ resources/js/script.js
@@ -1,107 +1,96 @@
+// Selecting necessary DOM elements
 const wrapper = document.getElementById("tiles");
+const pageWrapper = document.getElementById("page-wrapper");
+let columns = 0, rows = 0;
+let toggled = false, toggledOpacity = false;
 
-let columns = 0,
-    rows = 0,
-    toggled = false,
-    toggledOpacity = false;
-
+// Function to toggle the main animation
 const toggle = () => {
   toggled = !toggled;
-
-  document.body.classList.toggle("toggled");
+  pageWrapper.classList.toggle("toggled");
 }
 
-const toggleOpacity = () =>{
+// Function to toggle the opacity of the description
+const toggleOpacity = () => {
   toggledOpacity = !toggledOpacity;
-  if(toggledOpacity){
-    let des = document.getElementById('description')
-    des.style.opacity = "1"; 
-  }else{
-    let des = document.getElementById('description');
-    des.style.opacity = "0" ;
-  }
+  let des = document.getElementById('description');
+  des.style.opacity = toggledOpacity ? "1" : "0";
 }
 
+// Handle individual tile click
 const handleOnClick = index => {
   toggle();
   toggleOpacity();
-  
+
   anime({
     targets: ".tile",
     opacity: toggled ? 0 : 1,
-    delay: anime.stagger(50, {
-      grid: [columns, rows],
-      from: index
-    })
+    delay: anime.stagger(50, {grid: [columns, rows], from: index})
   });
 }
 
+// Word animation setup
 const words = ['Inspire', 'Educate', 'Elevate'].map(word => word.split(''));
-let currentWordIndex = 0;
-let currentLetterIndex = 0;
+let currentWordIndex = 0, currentLetterIndex = 0;
 
+// Animate the change of letters
 function animateLetterChange() {
   const description = document.getElementById('description');
   if (currentLetterIndex >= description.children.length) {
-    if (currentWordIndex < words.length - 1) {
-      currentWordIndex++;
-    } else {
-      currentWordIndex = 0;
-    }
+    currentWordIndex = (currentWordIndex < words.length - 1) ? currentWordIndex + 1 : 0;
     currentLetterIndex = 0;
-    description.classList.remove('fading'); // Remove the 'fading' class
-    setTimeout(animateLetterChange, 2000); // Wait for 2 seconds before starting the animation for the next word
+    description.classList.remove('fading');
+    setTimeout(animateLetterChange, 2000);
   } else {
-    description.classList.add('fading'); // Add the 'fading' class
+    description.classList.add('fading');
     const span = document.createElement('span');
     span.className = 'letter';
     span.textContent = words[currentWordIndex][currentLetterIndex];
     description.children[currentLetterIndex].replaceWith(span);
     currentLetterIndex++;
-    setTimeout(animateLetterChange, 100); // Continue the animation for the current word
+    setTimeout(animateLetterChange, 100);
   }
 }
 
+// Initialize the word animation on window load
 window.onload = function() {
   const description = document.getElementById('description');
   description.innerHTML = words[currentWordIndex].map(letter => `<span>${letter}</span>`).join('');
-  setTimeout(animateLetterChange, 2000); // Start the animation for the first word after 2 seconds
+  setTimeout(animateLetterChange, 2000);
+  createGrid();  // Create the initial grid
 };
 
+// Function to create a single tile
 const createTile = index => {
   const tile = document.createElement("div");
-  
   tile.classList.add("tile");
-  
   tile.style.opacity = toggled ? 0 : 1;
-  
   tile.onclick = e => handleOnClick(index);
-  
   return tile;
 }
 
+// Function to create multiple tiles
 const createTiles = quantity => {
-  Array.from(Array(quantity)).map((tile, index) => {
+  Array.from(Array(quantity)).forEach((_, index) => {
     wrapper.appendChild(createTile(index));
   });
 }
 
+// Function to create the grid of tiles
 const createGrid = () => {
   wrapper.innerHTML = "";
-  
-  const size = document.body.clientWidth > 800 ? 100 : 50;
-  
-  columns = Math.floor(document.body.clientWidth / size);
-  rows = Math.floor(document.body.clientHeight / size);
-  
+  const size = pageWrapper.clientWidth > 800 ? 100 : 50;
+  columns = Math.floor(pageWrapper.clientWidth / size);
+  rows = Math.floor(pageWrapper.clientHeight / size);
   wrapper.style.setProperty("--columns", columns);
   wrapper.style.setProperty("--rows", rows);
-  
   createTiles(columns * rows);
 }
 
-createGrid();
+// Handle window resizing to adjust the grid
+window.onresize = createGrid;
 
+// Dynamic title animation
 let direction = true;
 let title = "🚀";
 let name = 'FutureSpace';
@@ -110,19 +99,10 @@ let currentSpaces = 0;
 
 setInterval(() => {
   let spaces = "_".repeat(currentSpaces);
-  document.title = name+ spaces + title;
-
+  document.title = name + spaces + title;
   if (direction) {
-    currentSpaces++;
-    if (currentSpaces >= maxSpaces) {
-      direction = false;
-    }
+    currentSpaces = (currentSpaces >= maxSpaces) ? (direction = false, maxSpaces) : currentSpaces + 1;
   } else {
-    currentSpaces--;
-    if (currentSpaces <= 0) {
-      direction = true;
-    }
+    currentSpaces = (currentSpaces <= 0) ? (direction = true, 0) : currentSpaces - 1;
   }
 }, 500);
-
-window.onresize = () => createGrid();
